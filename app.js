02b869afcaff6150d632cb80dd29549994bc000f
Udi Shalev <azsd91@gmail.com>
2021-11-08 13:20:34 +0200

    Added cors handling and refactoring

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 4000;
 
+///Currently learning working with files after that i'll implement a DB with MongoDB
 const feedbacks = JSON.parse(
   fs.readFileSync(`${__dirname}/Feedbacks/Feedbacks.json`)
 );
+
 const currentuser = JSON.parse(
   fs.readFileSync(`${__dirname}/Users/CurrentUser.json`)
 );
 
+//Middlewares
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+//   next();
+// });
+
 app.use(express.json());
 
-//CURRENT USER HANDLER
-app.get('/currentuser', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.status(200).send(currentuser);
-});
+const error = (req, res) => res.status(404).json({ message: 'Not Found' });
 
 ///////FEEDBACKS HANDLERS
 //Getters
-app.get('/feedbacks', (req, res) => {
+const getAllFeedbacks = (req, res) => {
   // const items = feedbacks.map(feedback => {
   //   const { id, title, category, upvotes, status, description } = feedback;
   //   return { id, title, category, upvotes, status, description };
   // });
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.status(200).json(feedbacks);
-});
 
-app.get('/feedbacks/comments', (req, res) => {
+  res.status(200).json(feedbacks);
+};
+
+const getAllComments = (req, res) => {
   //prettier-ignore
   const items = feedbacks.filter(i => i.comments).map(feedback => feedback.comments);
 
   // const comments = items.map(item => item.map(i => i.content));
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.status(200).json(items);
-});
 
-app.get('/feedbacks/stats', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.status(200).json(items);
+};
+
+const getAllFeedbacksStats = (req, res) => {
   const stats = feedbacks?.map(fb => fb.status);
   if (stats) {
     res
@@ -48,11 +59,10 @@ app.get('/feedbacks/stats', (req, res) => {
       .json(req.query.unique === 'true' ? [...new Set(stats)] : stats);
     return;
   }
-  res.status(404).json({ message: 'Not Found' });
-});
+  error(req, res);
+};
 
-app.get('/feedbacks/:id', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+const getFeedbackByID = (req, res) => {
   const { id } = req.params;
   const feedback = feedbacks.find(fb => fb.id === +id);
   if (feedback) {
@@ -60,52 +70,40 @@ app.get('/feedbacks/:id', (req, res) => {
     res.status(200).json(feedback);
     return;
   }
-  res.status(404).json({ message: 'Not Found' });
-});
+  error(req, res);
+};
 
-app.get('/feedbacks/:id/comments', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+const getCommentsOfFeedbackByID = (req, res) => {
   const { id } = req.params;
   const feedback = feedbacks.find(fb => fb.id === +id);
-  if (feedback) {
+  if (feedback && +feedback.comments?.length > 0) {
     const { comments } = feedback;
     comments && res.status(200).json(comments);
     return;
   }
-  res.status(404).json({ message: 'Not Found' });
-});
+  error(req, res);
+};
 
-app.get('/feedbacks/:id/title', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  const { id } = req.params;
-  const feedback = feedbacks.find(fb => fb.id === +id);
-
-  if (feedback) {
-    const { title } = feedback;
-    title && res.status(200).json({ title });
+const addFeedback = (req, res) => {
+  if (req.body) {
+    feedbacks.push(req.body);
+    fs.writeFile(
+      `${__dirname}/Feedbacks/Feedbacks.json`,
+      JSON.stringify(feedbacks),
+      err => {
+        res.send('done');
+      }
+    );
     return;
   }
-  res.status(404).json({ message: 'Not Found' });
-});
+  error(req, res);
+};
 
-//Posters
-app.post('/feedbacks/addfeedback', (req, res) => {
-  feedbacks.push(req.body);
-  fs.writeFile(
-    `${__dirname}/Feedbacks/Feedbacks.json`,
-    JSON.stringify(feedbacks),
-    err => {
-      res.send('done');
-    }
-  );
-});
-
-//Delete
-app.delete('/feedbacks/delete/:id', (req, res) => {
+const deleteFeedbackByID = (req, res) => {
   const { id } = req.params;
   const feedbackID = feedbacks.find(i => i.id === +id);
-  const itemIndex = feedbacks.indexOf(feedbackID);
-  if (feedbackID) {
+  if (feedbackID && id) {
+    const itemIndex = feedbacks.indexOf(feedbackID);
     feedbacks.splice(itemIndex, 1);
     fs.writeFile(
       `${__dirname}/Feedbacks/Feedbacks.json`,
@@ -114,9 +112,26 @@ app.delete('/feedbacks/delete/:id', (req, res) => {
         res.send('done');
       }
     );
+    return;
   }
+  error(req, res);
+};
+
+//CURRENT USER HANDLER
+// i know this is extreamly bad implementaion, im just learning how to work with files and basic express commands
+app.get('/currentuser', (req, res) => {
+  res.status(200).send(currentuser);
 });
 
+app.get('/feedbacks/stats', getAllFeedbacksStats);
+
+app.get('/feedbacks/:id/comments', getCommentsOfFeedbackByID);
+
+app.get('/feedbacks/comments', getAllComments);
+app.route('/feedbacks').get(getAllFeedbacks).post(addFeedback);
+
+app.route('/feedbacks/:id').get(getFeedbackByID).delete(deleteFeedbackByID);
+
 app.listen(port, () => {
-  console.log(`Running http://127.0.0.1:${port}`);
+  console.log(`Running http://localhost:${port}`);
 });
